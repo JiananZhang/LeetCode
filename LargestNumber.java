4a19d1c485e0c53dbcaed732280514dcae2e527b
Zhang Eddie <eddie@Zhangs-MacBook-Pro.local>
2021-01-23 10:48:04 -0800

    Finally recall

--- LargestNumber.java
+++ LargestNumber.java
@@ -1,7 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 给定一个数组，求本身数值与其出现次数相等的那些数里最大的那个。用哈希表即可。代码如下：
  */
-public class Solution{
+public class LargestNumber{
     public int findX(int[] arr){
         if(arr == null || arr.length == 0){
             return 0;
@@ -9,7 +12,7 @@ public class Solution{
 
         Map<Integer, Integer> map = new HashMap<>();
         for (int x : arr){
-            map.put(x, map.getOrDeFault(x, 0) + 1);
+            map.put(x, map.getOrDefault(x, 0) + 1);
         }
 
         int res = 0;
