357da69b1275728a27137b4c9d97250c7bbc0f83
Zhang Eddie <eddie@Zhangs-MacBook-Pro.local>
2021-01-23 10:40:16 -0800

    Finished version

--- LargestNUmber.java
+++ LargestNUmber.java
@@ -1,3 +1,24 @@
+/*
+给定一个数组，求本身数值与其出现次数相等的那些数里最大的那个。用哈希表即可。代码如下：
+ */
 public class Solution{
-    
+    public int findX(int[] arr){
+        if(arr == null || arr.length == 0){
+            return 0;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int x : arr){
+            map.put(x, map.getOrDeFault(x, 0) + 1);
+        }
+
+        int res = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()){
+            int key = entry.getKey(), value = entry.getValue();
+            if(key == value) {
+                res = Math.max(res, key);
+            }
+        }
+        return res;
+    }
 }
